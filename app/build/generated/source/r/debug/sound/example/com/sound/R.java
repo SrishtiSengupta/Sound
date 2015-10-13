b710226078e0569f6ad5b694b0e49fbf7d6c3f88
Srishti Sengupta <srishti.sengupta@gmail.com>
2015-10-13 20:17:48 +0530

    Audio Sampling done

--- app/build/generated/source/r/debug/sound/example/com/sound/R.java
+++ app/build/generated/source/r/debug/sound/example/com/sound/R.java
@@ -15,10 +15,7 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button_play=0x7f080003;
-        public static final int button_record=0x7f080001;
-        public static final int button_stop=0x7f080002;
+        public static final int action_settings=0x7f080001;
         public static final int textView=0x7f080000;
     }
     public static final class layout {
--- app/src/main/java/sound/example/com/sound/MainActivity.java
+++ app/src/main/java/sound/example/com/sound/MainActivity.java
@@ -1,112 +1,98 @@
 package sound.example.com.sound;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends Activity {
-    Button record, play, stop;
-    private MediaRecorder myAudioRecorder;
-    private String outputFile = null;
+
+    private MediaRecorder mRecorder = null;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        record = (Button) findViewById(R.id.button_record);
-        play = (Button) findViewById(R.id.button_play);
-        stop = (Button) findViewById(R.id.button_stop);
+        handler = new Handler();
 
-        stop.setEnabled(false);
-        play.setEnabled(false);
-        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
-        ;
-
-        myAudioRecorder = new MediaRecorder();
-        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        myAudioRecorder.setOutputFile(outputFile);
-
-        record.setOnClickListener(new View.OnClickListener() {
+        Runnable runnable = new Runnable() {
             @Override
-            public void onClick(View view) {
-                try {
-                    myAudioRecorder.prepare();
-                    myAudioRecorder.start();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (IllegalStateException e) {
-                    e.printStackTrace();
-                }
-
-                record.setEnabled(false);
-                stop.setEnabled(true);
-
-                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+            public void run() {
+                startRecording();
+                Log.d("RECORDING", " started");
+                handler.postDelayed(this, 5000);
             }
-        });
+        };
 
-        stop.setOnClickListener(new View.OnClickListener() {
+        Runnable runnable_ = new Runnable() {
             @Override
-            public void onClick(View view) {
-                myAudioRecorder.stop();
-                myAudioRecorder.release();
-                myAudioRecorder = null;
-
-                stop.setEnabled(false);
-                play.setEnabled(true);
-                Toast.makeText(getApplicationContext(), "Audio recorded successfully", Toast.LENGTH_LONG).show();
+            public void run() {
+                stopRecording();
+                Log.d("RECORDING", " stopped");
+                handler.postDelayed(this, 10000);
             }
-        });
+        };
 
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) throws IllegalArgumentException, SecurityException, IllegalStateException {
-                MediaPlayer m = new MediaPlayer();
+        try {
+            handler.postDelayed(runnable, 5000);
+            handler.postDelayed(runnable_, 10000);
+        }catch (Exception e){
+            Log.d("RECORDING", " Corrupted!");
+        }
+    }
 
-                try {
-                    m.setDataSource(outputFile);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    m.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                m.start();
-                Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
+    public void startRecording(){
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String timestamp = dateFormat.format(new Date());
+
+        String outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording_" + timestamp + ".3gp";
+
+        if (mRecorder == null) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile(outputFile);
+
+            try {
+                mRecorder.prepare();
+                mRecorder.start();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+        }
+    }
+
+    public void stopRecording(){
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
